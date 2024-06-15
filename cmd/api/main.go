a657c821fa1a3549e568684b9fe1c1e2407d7079
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-06-15 17:45:47 +0800

    add makefile

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
-	"log"
+	"fmt"
 	"os"
 	"runtime"
 	"strings"
@@ -15,11 +15,13 @@ import (
 	"github.com/EaRthSaVior/greenlight/internal/data"
 	"github.com/EaRthSaVior/greenlight/internal/jsonlog"
 	"github.com/EaRthSaVior/greenlight/internal/mailer"
-	"github.com/joho/godotenv"
+	"github.com/EaRthSaVior/greenlight/internal/vcs"
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -56,17 +58,12 @@ type application struct {
 }
 
 func main() {
-	godotenv.Load()
-	dbUrl := os.Getenv("ADMIN_DB_URL")
-	if dbUrl == "" {
-		log.Fatal("Db url is not found in env")
-	}
 
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", dbUrl, "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
@@ -85,7 +82,13 @@ func main() {
 		cfg.cors.trustedOrigins = strings.Fields(val)
 		return nil
 	})
+	displayVersion := flag.Bool("version", false, "Display version and exit")
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 	db, err := openDB(cfg)
 	if err != nil {
